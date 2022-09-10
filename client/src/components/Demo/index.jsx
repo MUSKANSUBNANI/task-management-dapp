ecb6ffb869d09e340c5efbea1d4796455590bee6
Muskan Subnani <muskansubnani98@gmail.com>
2022-09-10 17:19:09 -0500

    save front end work

--- client/src/components/Demo/index.jsx
+++ client/src/components/Demo/index.jsx
@@ -1,37 +1,104 @@
-import { useState } from "react";
+import { useState} from "react";
 import useEth from "../../contexts/EthContext/useEth";
-import Title from "./Title";
-import Cta from "./Cta";
-import Contract from "./Contract";
-import ContractBtns from "./ContractBtns";
-import Desc from "./Desc";
-import NoticeNoArtifact from "./NoticeNoArtifact";
-import NoticeWrongNetwork from "./NoticeWrongNetwork";
-
+import Task from "./Task";
 function Demo() {
   const { state } = useEth();
-  const [value, setValue] = useState("?");
+  const [inputValue, setInputValue] = useState("");
+  const [tasks,setTasks]=useState([]);
+
+  const handleInputChange = e => {
+    setInputValue(e.target.value);
+    
+  };
+
+  const getAllTasks = async() => {
+    try {
+        const TaskContract = state.contract;
+        console.log(TaskContract);
+        let allTasks = await TaskContract.methods.getMyTasks().call({ from: state.accounts[0] });
+        console.log(allTasks);
+        setTasks(allTasks);
+      } catch(error) {
+      console.log(error);
+    }
+  }
+
+
+  const write = async e => {
+    if (e.target.tagName === "INPUT") {
+      return;
+    }
+    if (inputValue === "") {
+      alert("Please enter a value to write.");
+      return;
+    }
+
+    const isComplete =false;
+    console.log( inputValue);
+    console.log( isComplete);
+    const taskText = inputValue;
+    
+    const result= await state.contract.methods.addTask(taskText, isComplete).send({ from:state.accounts[0] });
+    console.log(result);
+  };
+  
+  const deleteTask = key => async() => {
+    console.log(key);
+
+    // Now we got the key, let's delete our tweet
+    try {
+      
+        let deleteTaskTx = await state.contract.methods.deleteTask(key, true).send({ from:state.accounts[0] });
+        let allTasks = await state.contract.methods.getMyTasks().call({ from: state.accounts[0] });
+        setTasks(allTasks);
+      }
+
+    catch(error) {
+      console.log(error);
+    }
+  }
+
+
 
   const demo =
-    <>
-      <Cta />
-      <div className="contract-container">
-        <Contract value={value} />
-        <ContractBtns setValue={setValue} />
-      </div>
-      <Desc />
-    </>;
+  <div className="btns">
 
-  return (
-    <div className="demo">
-      <Title />
-      {
-        !state.artifact ? <NoticeNoArtifact /> :
-          !state.contract ? <NoticeWrongNetwork /> :
-            demo
-      }
-    </div>
-  );
+      <button onClick={getAllTasks}>
+        getAllmyTasks
+      </button>
+
+      <div onClick={write} className="input-btn">
+
+        write(<input
+          type="text"
+          placeholder="my new Task"
+          value={inputValue}
+          onChange={handleInputChange}
+        />)
+
+      </div>
+
+      tasks ? <ul>
+          {tasks.map(item=> 
+            <Task 
+              key={item.id} 
+              taskText={item.taskText} 
+              onClick={deleteTask(item.id)}
+            />)
+          }
+      </ul>: <div></div>
+
+    </div>;
+
+return (
+
+  <div className="demo">
+    {
+      demo
+    }
+  </div>
+);
+
 }
 
 export default Demo;
